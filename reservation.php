3f95ba9a454ff634fdfb416634843bfac6325df9
khirou <khiroumezerre@gmail.com>
2022-09-27 12:42:53 +0200

    amelioration reservation

--- reservation.php
+++ reservation.php
@@ -1,14 +1,12 @@
 <?php
 require_once('./include/init.php');
 
-
-
-
-
+if(!internauteConnecteAdmin() || !internauteConnecte()){
+    header('location:' . URL . 'connexion.php');
+    exit();
+}
 
 require_once('./include/header.php');
-
-
 
 ?>
 
